8acc5798c9ff3d72bf570c5ca911e11032916ba4
Carcara <daniel.n.assada@hotmail.com>
2020-06-06 23:22:24 -0300

    update our menu-items to navegate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -6,7 +9,10 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/h' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
